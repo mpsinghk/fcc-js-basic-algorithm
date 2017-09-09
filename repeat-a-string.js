1b51bdf8819ea53b7fe2bafc38e1849070841de5
mpsinghk <mpsinghk@users.noreply.github.com>
2017-09-09 16:24:01 -0700

    Added repeat-a-string.js

---
+++ repeat-a-string.js
@@ -0,0 +1,28 @@
+/**
+ * Repeat a given string (first argument) num times (second argument).
+ * Return an empty string if num is not a positive number.
+ */
+
+// Method 1
+function repeatStringNumTimes(str, num) {
+    if (num > 0) {
+        return str.repeat(num);
+    }
+    return "";
+}
+
+// Method 2
+function repeatStringNumTimes(str, num) {
+    return (num > 0) ? str.repeat(num) : "";
+}
+
+// Method 3
+function repeatStringNumTimes(str, num) {
+    if (num <= 0) {
+        return "";
+    } else {
+        return str + repeatStringNumTimes(str, num - 1);
+    }
+}
+
+repeatStringNumTimes("abc", 1);
