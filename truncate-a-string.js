36d96e7a01d80b24f7b0f1129c359d917a888830
Maninder Pal Singh <mpsinghk@users.noreply.github.com>
2017-09-25 15:37:55 -0700

    Minor update

--- truncate-a-string.js
+++ truncate-a-string.js
@@ -16,7 +16,6 @@
 
 function truncateString(str, num) {
     // Clear out that junk in your trunk
-
     if ( str.length > num && num > 3 ) {
         return str.slice(0, num - 3) + "...";
     } else if ( str.length > num && num <= 3 ) {
